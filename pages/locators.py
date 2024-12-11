8ed254c46ae363adf821728b201df834205239b7
karie <kari.egorova2013@yandex.ru>
2024-12-11 14:07:14 +0300

    Add product page test and research for bags

--- pages/locators.py
+++ pages/locators.py
@@ -5,3 +5,11 @@ class LoginPageLocators:
     LOGIN_FORM = ('xpath', '//form[@id="login_form"]')
     REGISTER_FORM = ('xpath', '//form[@id="register_form"]')
 
+class ProductPageLocators:
+    ADD_TO_CARD_BUTTON = ('xpath', '(//button[@type="submit"])[2]')
+    ADDED_TO_CARD = ('xpath', '(//strong)[4]')
+    COUNT_CARD = ('xpath', '(//strong)[6]')
+    HEAD_TITLE = ('xpath', '//h1')
+    PRICE = ('xpath', '//p[@class="price_color"]')
+
+
---
+++ pages/product_page.py
@@ -0,0 +1,32 @@
+from .base_page import BasePage
+from .locators import ProductPageLocators
+
+
+
+class ProductPage(BasePage):
+    def add_product_to_card(self):
+        self.browser.find_element(*ProductPageLocators.ADD_TO_CARD_BUTTON).click()
+        self.solve_quiz_and_get_code()
+        self.should_be_success_message()
+        self.should_be_same_titles()
+        self.should_be_message_with_price()
+        self.should_be_same_prices()
+
+    def should_be_success_message(self):
+        element = self.is_element_present(*ProductPageLocators.ADDED_TO_CARD)
+        assert element, "There is no success message"
+
+    def should_be_same_titles(self):
+        element_title = self.browser.find_element(*ProductPageLocators.ADDED_TO_CARD).text
+        book_title = self.browser.find_element(*ProductPageLocators.HEAD_TITLE).text
+        assert element_title == book_title, "The book titles don't match"
+
+    def should_be_message_with_price(self):
+        element = self.is_element_present(*ProductPageLocators.COUNT_CARD)
+        assert element, "There is no message with card price"
+
+    def should_be_same_prices(self):
+        element_price = self.browser.find_element(*ProductPageLocators.COUNT_CARD).text
+        book_price = self.browser.find_element(*ProductPageLocators.PRICE).text
+        assert element_price == book_price, "The book prices don't match"
+    
